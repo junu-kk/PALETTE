d1103fe54e82590d4d37c2b2859c9e2587c1ec38
brian <brianjune@naver.com>
2019-10-06 16:12:57 +0900

    login signup res.json

--- server/routes/authentication/login.js
+++ server/routes/authentication/login.js
@@ -13,8 +13,16 @@ router.post('/', passport.authenticate('local-login',{
   failureRedirect:'/login',
   failureFlash:true,
 }), (req,res)=>{
-  console.log(req.user.is_new);
-  res.send(req.user.is_new);
+  var fmsg=req.flash();
+  var feedback='';
+  if(fmsg.error){
+    feedback=fmsg.error[0];
+    res.json(0,fmsg);
+  } else{
+    res.json(1);
+  }
 });
+
+router.post('/',passport.authenticate('local-login',))
 
 module.exports = router;
--- server/routes/authentication/signup.js
+++ server/routes/authentication/signup.js
@@ -4,7 +4,20 @@ var router = express.Router();
 var passport = require('passport');
 
 //for signup procedure
-router.post('/', passport.authenticate('local-signup',(req,res)=>console.log(res)));
+//router.post('/', passport.authenticate('local-signup',(req,res)=>console.log(res)));
 
+router.post('/', passport.authenticate('local-signup',{
+  failureRedirect:'/login',
+  failureFlash:true,
+}),(req,res)=>{
+  var fmsg=req.flash();
+  var feedback='';
+  if(fmsg.error){
+    feedback=fmsg.error[0];
+    res.json(0,fmsg);
+  } else{
+    res.json(1);
+  }
+});
 
 module.exports = router;
