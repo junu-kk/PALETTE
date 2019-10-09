f8ded665685405676076cb641d985362e290a9ac
brian <brianjune@naver.com>
2019-10-09 23:59:58 +0900

    first_login change

--- server/routes/first_login.js
+++ server/routes/first_login.js
@@ -17,25 +17,27 @@ var User = require('../models/User');
 router.post('/', (req,res,next)=>{
   if(req.isUnauthenticated()){
     res.json(0,"not logged in");
+  } else{
+    User.findOneAndUpdate({email:req.user.email}, {
+      dob:req.body.dob,
+      address:req.body.address,
+      bio:req.body.bio,
+      grade:req.body.grade,
+      class:req.body.class,
+      work_exp:req.body.work_exp,
+      fun_facts:req.body.fun_facts,
+    }, (err,updateUser)=>{
+      if(err) throw err;
+      updateUser.saveUser();
+    });
+    res.json(1);
   }
   /*
   if(req.isUnauthenticated()){
     return res.redirect('/login');
   }
   */
-  User.findOneAndUpdate({email:req.user.email}, {
-    dob:req.body.dob,
-    address:req.body.address,
-    bio:req.body.bio,
-    grade:req.body.grade,
-    class:req.body.class,
-    work_exp:req.body.work_exp,
-    fun_facts:req.body.fun_facts,
-  }, (err,updateUser)=>{
-    if(err) throw err;
-    updateUser.saveUser();
-  });
-  res.json(1);
+  
 });
 
 
