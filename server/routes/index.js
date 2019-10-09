ab5907f28688bb9f118f0988cdda24aa048c7c22
brian <brianjune@naver.com>
2019-10-10 00:06:58 +0900

    user info

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 router.get('/', (req,res,next)=>{
   res.send('working');
 });
+
+router.get('/user', (req,res,next)=>{
+  if(req.isUnauthenticated()){
+    res.json(0,"not logged in");
+  } else{
+    User.findOne({email:req.user.email},(err,user)=>{
+      if(err) throw err;
+      res.json(1,user);
+    })
+  }
+})
 
 module.exports=router;
