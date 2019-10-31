8f6f1844a788a8bb415c56deef055a6f56768951
brian <brianjune@naver.com>
2019-11-01 02:10:32 +0900

    temp

--- client/src/containers/SchoolContainer/SchoolContainer.js
+++ client/src/containers/SchoolContainer/SchoolContainer.js
@@ -4,10 +4,10 @@ import School from '../../components/School';
 import axios from 'axios';
 
 const SchoolContainer = ({school})=>{
-  const getSchoolInfo=()=>{
+  const getSchoolInfo=() => {
     return axios.get('http://127.0.0.1:5000/school').then(response=>{
       console.log(response);
-      return response;//왜 response를 가져오지 못하는가?
+      return response;
     }).catch(error=>{
       alert('failed to load school information');
       throw error;
---
+++ server/newModels/Application.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var applicationSchema=mongoose.Schema({
+  form:{
+    type:String,
+  },
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+  },
+  competition:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Competition',
+  },
+  event:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Event',
+  },
+  club:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Club',
+  }
+}, {collection:'application'});
+
+applicationSchema.methods={
+  saveApplication: function(callback){
+    var self=this;
+
+    this.validate(function(err){
+      if(err) return callback(err);
+      self.save(callback);
+    });
+  }
+}
