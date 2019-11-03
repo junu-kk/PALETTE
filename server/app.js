86d1c09c045a217beb51c3dde54d20e238093998
brian <brianjune@naver.com>
2019-11-04 00:11:21 +0900

    LOGIN SUCCESS~!~!~!~!

--- server/app.js
+++ server/app.js
@@ -30,6 +30,7 @@ var noticeRouter = require('./newroutes/notice');
 var portfolioRouter = require('./newroutes/portfolio');
 var schoolRouter = require('./newroutes/school');
 var userRouter = require('./newroutes/user');
+var loginRouter = require('./newroutes/login');
 
 var app = express();
 
@@ -39,10 +40,10 @@ library.passport();
 library.database();
 
 // view engine setup
-/*
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-*/
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -68,6 +69,7 @@ app.use('/notice', noticeRouter);
 app.use('/portfolio', portfolioRouter);
 app.use('/school', schoolRouter);
 app.use('/user', userRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/newRoutes/login.js
@@ -0,0 +1,31 @@
+//router for login
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
+//for login procedure
+
+// router.post('/', passport.authenticate('local-login'), (req,res)=>console.log(res));
+
+
+router.post('/', function (req, res, next) {
+  passport.authenticate('local-login', {session: false}, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something is not right',
+        user   : user
+      });
+    }
+    req.login(user, {session: false}, (err) => {
+      if (err) {
+        res.send(err);
+      }
+      const token = jwt.sign(user.toJSON(), 'testsecret');
+      return res.json({user, token});
+    });
+  })(req, res);
+});
+
+router.post('/',passport.authenticate('local-login',));
+
+module.exports = router;
