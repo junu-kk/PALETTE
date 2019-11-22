4225af808151fcbe7234d95d1984e5eb97c5f839
paramecium6 <paramecium6@gmail.com>
2019-11-22 11:05:28 +0900

    Example and firstsignin

--- client/src/containers/FirstSignInContainer/FirstSignInContainer.js
+++ client/src/containers/FirstSignInContainer/FirstSignInContainer.js
@@ -4,8 +4,14 @@ import React from 'react';
 import FirstSignIn from '../../components/FirstSignIn';
 
 const FirstSignInContainer = ({history}) => {
-    const handleSubmit = (dateOfBirth, address, introduction, grade, _class, workExperience, funFacts) => dispatch => {
-        return axios.post('http://127.0.0.1:5000/first_login/',{dob:dateOfBirth, address:address, bio:introduction, grade:grade, class: _class, work_exp:workExperience, fun_facts: funFacts})
+    const handleSubmit = (dateOfBirth, address, introduction, grade, _class, workExperience, funFacts) => {
+        const token = sessionStorage.getItem('token');
+        return axios.post('http://127.0.0.1:5000/user/update/',{dob:dateOfBirth, address:address, bio:introduction, grade:grade, class: _class, work_exp:workExperience, fun_facts: funFacts},
+            {
+                headers:{
+                    'Authorization': token
+                }
+            })
             .then(response => {
                 console.log(response);
                 history.push('/mypage');
--- client/src/containers/MyPageContainer/MyPageContainer.js
+++ client/src/containers/MyPageContainer/MyPageContainer.js
@@ -2,23 +2,25 @@ import axios from 'axios';
 import React from 'react';
 import MyPage from '../../components/MyPage';
 
-const MyPageContainer = ({userInfo}) => {
-    const getUserInfo = () => {
+const MyPageContainer = () => {
+    const [userInfo, setUserInfo] = React.useState();
+
+    const getUserInfo = async () => {
         const token = sessionStorage.getItem('token');
-        return axios.get('http://127.0.0.1:5000/user/my', {
-            headers: {
-                'Authorization': token
-            }
-        }).then(response => {
-            console.log(response);
-            return response;
-        }).catch(error => {
-            alert('failed to load user information.');
-            throw error;
-        })
+        try {
+            const temp = await axios.get('http://127.0.0.1:5000/user/my', {
+                headers: {
+                    'Authorization': token
+                }
+            });
+            console.log(temp);
+            setUserInfo(temp)
+        } catch(error) {
+            console.log(error)
+        }
     };
 
-    return (<MyPage onGetUserInfo={getUserInfo}/>)
+    return (<MyPage onGetUserInfo={getUserInfo} userInfo={userInfo}/>)
 };
 
 export default MyPageContainer;
