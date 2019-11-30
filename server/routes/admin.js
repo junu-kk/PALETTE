babb0932e1390270f6520756e0df5de054e68f08
brian <brianjune@naver.com>
2019-11-30 23:16:58 +0900

    set admin router, to upload schoolclubexc easily

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -10,37 +10,10 @@ var User = require('../models/User');
 var Exc = require('../models/Exc');
 var School = require('../models/School');
 var Club = require('../models/Club');
-var path = require('path');
-var crypto = require('crypto');
 var multer = require('multer');
-var GridFsStorage = require('multer-gridfs-storage');
-var Grid = require('gridfs-stream');
+var upload = multer({dest:'./upload'});
 
-var mongoose = require('mongoose');
-var mongoURI = 'mongodb+srv://KangJunewoo:brian980115@cluster0-mh67x.mongodb.net/palette_test?retryWrites=true&w=majority';
-const conn = mongoose.createConnection(mongoURI);
-let gfs;
-conn.once('open', ()=>{
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
-});
 
-const storage = new GridFsStorage({
-  url:'mongodb+srv://KangJunewoo:brian980115@cluster0-mh67x.mongodb.net/palette_test?retryWrites=true&w=majority',
-  file:(req,file)=>{
-    return new Promise((resolve, reject)=>{
-      crypto.randomBytes(16, (err,buf)=>{
-        if(err) return reject(err);
-        const filename = buf.toString('hex')+path.extname(file.originalname);
-        const fileInfo = {
-          filename:filename,
-          bucketName:'uploads',
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
 /*
 function adminCheck(req,res){
   User.findOne({email:req.user.email}).exec((err,user)=>{
@@ -55,7 +28,6 @@ function adminCheck(req,res){
   });
 }
 */
-const upload = multer({storage});
 
 router.get('/', (req,res)=>{
   if(req.isUnauthenticated()){
@@ -108,13 +80,13 @@ router.get('/exc/create', (req,res,next)=>{
 });
 
 router.post('/exc/create', upload.single('file'), (req,res,next)=>{
+  let image = '/image/'+req.file.filename;
   var newExc = new Exc();
   //console.log(res.req); 여기서 file이 없음.
   //왜 이게 안되는가.
   //다른거는 mypage는 file만 보내는거고 여기는 다 보내는건데.
   //mypage처럼 하나만 딱 해보자.
-  newExc.pic = res.req.file.id;
-  
+  newExc.pic = image;
   newExc.name = req.body.name;
   newExc.info = req.body.info;
   newExc.sch.event = req.body.sche;
@@ -125,23 +97,6 @@ router.post('/exc/create', upload.single('file'), (req,res,next)=>{
   newExc.palette = req.body.palette;
   newExc.gu = req.body.gu;
   newExc.location = req.body.location;
-
-  if(req.body.apcnq0!=null){
-    newExc.apcnqs.push(req.body.apcnq0);
-  }
-  if(req.body.apcnq1!=null){
-    newExc.apcnqs.push(req.body.apcnq1);
-  }
-  if(req.body.apcnq2!=null){
-    newExc.apcnqs.push(req.body.apcnq2);
-  }
-  if(req.body.apcnq3!=null){
-    newExc.apcnqs.push(req.body.apcnq3);
-  }
-  if(req.body.apcnq4!=null){
-    newExc.apcnqs.push(req.body.apcnq4);
-  }
-
   
   newExc.saveExc((err)=>{
     if(err) throw err;
@@ -204,18 +159,16 @@ router.get('/school/create', (req,res,next)=>{
 });
 
 router.post('/school/create', upload.single('file'), (req,res,next)=>{
+  let image = '/image/'+req.file.filename;
+  
   var newSchool = new School();
-  newSchool.pic = res.req.file.id;
+  newSchool.pic = image;
   newSchool.name = req.body.name;
   newSchool.address = req.body.address;
   newSchool.info = req.body.info;
-  console.log(newSchool);
+  
   newSchool.saveSchool((err)=>{
     if(err) throw err;
-  });
-  School.find({}).exec((err,schools)=>{
-    if(err) throw err;
-    console.log(schools);
   });
   return res.redirect('/admin/school');
 
@@ -279,15 +232,14 @@ router.get('/club/create', (req,res,next)=>{
 });
 
 router.post('/club/create', upload.single('file'), (req,res,next)=>{
+  let image = '/image/'+req.file.filename
   var newClub = new Club();
-  newClub.pic = res.req.file.id;
+  newClub.pic = image;
 
   newClub.name = req.body.name;
   newClub.info = req.body.info;
   newClub.school = req.body.school;
   
-  console.log(req.body.school);
-  console.log(newClub.school);
   
   newClub.saveClub((err)=>{
     if(err) throw err;
@@ -317,7 +269,7 @@ router.get('/club/delete/:id', (req,res)=>{
       return res.redirect('/main');
     }
   });
-  //Club.findOneAndDelete({_id:req.params.id},(err)=>{if(err) throw err;});
+
   Club.findOne({_id:req.params.id}).exec((err,club)=>{
     if(err) throw err;
 
